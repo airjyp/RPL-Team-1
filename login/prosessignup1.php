639f091e9d6cfb222557e5d6c1904afbb74df066
airjyp <valldyarco@gmail.com>
2017-05-22 19:30:40 +0700

    udah nambahin kalo dia username atau email already exist. coba cek yang vendor

--- login/prosessignup1.php
+++ login/prosessignup1.php
@@ -9,6 +9,23 @@ if($_POST) {
 	$email_cust = $_POST['email_cust'];
 	$username_cust = $_POST['username_cust'];
 	$password_cust = $_POST['password_cust'];
+	$usercheck = mysqli_query($connect,"SELECT * FROM customer WHERE username_cust = '$username_cust'");
+	$emailcheck = mysqli_query($connect,"SELECT * FROM customer WHERE email_cust = '$email_cust'");
+	if(mysqli_num_rows($usercheck) > 1){ ?>
+			<script language="javascript">alert("Username is already taken!");</script>
+        	<script>document.location.href='../login/signup.php';</script>
+            $еrror = true;
+            <?php
+    }
+
+    else if(mysqli_num_rows($emailcheck) > 1){ ?>
+			<script language="javascript">alert("email is already exist!");</script>
+        	<script>document.location.href='../login/signup.php';</script>
+            $еrror = true;
+            <?php
+    }
+
+    else {
 	$sql = "INSERT INTO customer (id_cust, fname_cust, lname_cust, telephone_cust, email_cust, address_cust, username_cust, password_cust, active) VALUES ('', '$fname_cust', '$lname_cust', '$telephone_cust', '$email_cust', '','$username_cust', '$password_cust' ,1) ";
 	if($connect->query($sql) === TRUE) {
 
@@ -24,5 +41,5 @@ if($_POST) {
 
 	$connect->close();
 }
-
+}
 ?>
--- login/prosessignup2.php
+++ login/prosessignup2.php
@@ -1,5 +1,5 @@
 <?php
-
+	session_start();
 require_once 'db_connect.php';
 
 if($_POST) {
@@ -9,6 +9,23 @@ if($_POST) {
 	$email_vendor = $_POST['email_vendor'];
 	$username_vendor = $_POST['username_vendor'];
 	$password_vendor = $_POST['password_vendor'];
+	$usercheck = mysqli_query($connect,"SELECT * FROM vendor WHERE username_vendor = '$username_vendor'");
+	$emailcheck = mysqli_query($connect,"SELECT * FROM vendor WHERE email_vendor = '$email_vendor'");
+	if(mysqli_num_rows($usercheck) > 1){ ?>
+			<script language="javascript">alert("Username is already taken!");</script>
+        	<script>document.location.href='../login/signup.php';</script>
+            $еrror = true;
+            <?php
+    }
+    
+    else if(mysqli_num_rows($emailcheck) > 1){ ?>
+			<script language="javascript">alert("email is already exist!");</script>
+        	<script>document.location.href='../login/signup.php';</script>
+            $еrror = true;
+            <?php
+    }
+
+    else {
 	$sql = "INSERT INTO vendor (id_vendor, fname_vendor, lname_vendor, telephone_vendor, email_vendor, address_vendor, id_category, amount, username_vendor, password_vendor, active) VALUES ('', '$fname_vendor', '$lname_vendor', '$telephone_vendor', '$email_vendor', '', '', '', '$username_vendor', '$password_vendor' ,1) ";
 	if($connect->query($sql) === TRUE) {
 		?>
@@ -22,5 +39,5 @@ if($_POST) {
 
 	$connect->close();
 }
-
+}
 ?>
